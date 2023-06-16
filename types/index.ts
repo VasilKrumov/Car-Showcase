9954c9534a6a018e8939e9c9383cf3b3e644cf93
vasil.krumov <vassil.krumov@gmail.com>
2023-06-16 11:06:04 +0300

    Added header and footer

--- types/index.ts
+++ types/index.ts
@@ -4,4 +4,5 @@ export interface CustomButtonProps {
   title: string;
   handleClick?: MouseEventHandler<HTMLButtonElement>;
   containerStyles?: string;
+  btnType: "button" | "submit" | "reset";
 }
