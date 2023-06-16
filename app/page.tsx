b09e1c7d2842770b23bb9fa5ba978cfb2b7680b2
vasil.krumov <vassil.krumov@gmail.com>
2023-06-16 17:07:38 +0300

    Added SearchManufacturer.tsx component
    Car Card Work in Progress
    
    Remove secret
    
    Added .env.local

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
-import { Hero } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { getCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await getCars();
+  const isDataEmpty = !Array.isArray(allCars) || !allCars.length || !allCars;
   return (
     <main className="overflow-hidden">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id={"discover"}>
+        <div className={"home__text-container"}>
+          <h1 className={"text-4xl font-extrabold"}>Car Catalogue</h1>
+          <p>Explore the cars you might like</p>
+        </div>
+        <div className={"home__filters"}>
+          <SearchBar />
+          <div className="home__filter-container">
+            <CustomFilter />
+            <CustomFilter />
+          </div>
+        </div>
+      </div>
+      {!isDataEmpty ? (
+        <section>
+          <div className="home__cars-wrapper padding-x padding-y">
+            {allCars?.map((car, index) => (
+              <CarCard car={car} key={index} />
+            ))}
+          </div>
+        </section>
+      ) : (
+        <div className="home__error-container">
+          <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+          <p>{allCars?.message}</p>
+        </div>
+      )}
     </main>
   );
 }
